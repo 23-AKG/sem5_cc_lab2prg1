133667ac52b1321ca56ddcc600c00f33953c1234
23-AKG <akarshkumargowda23@gmail.com>
2024-08-22 23:14:33 +0530

    Refactor lab2_prg1.py and add test_lab2_prg1.py

--- lab2_prg1.py
+++ lab2_prg1.py
@@ -1,15 +1,20 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sklearn
+import matplotlib.pyplot as plt
 from sklearn import linear_model
-height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
-weight=[  16, 25 , 36, 49, 64, 81, 100]
-plt.scatter(height,weight,color='black')
-plt.xlabel("height")
-plt.ylabel("weight")
-reg=linear_model.LinearRegression()
-reg.fit(height,weight)
-X_height=[[12.0]]
-print(reg.predict(X_height))
+
+def train_and_predict(height_values, weight_values, new_height):
+    reg = linear_model.LinearRegression()
+    reg.fit(height_values, weight_values)
+    return reg.predict([[new_height]])
+
+if __name__ == "__main__":
+    height = [[4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0]]
+    weight = [16, 25, 36, 49, 64, 81, 100]
+    
+    plt.scatter(height, weight, color='black')
+    plt.xlabel("Height")
+    plt.ylabel("Weight")
+    plt.show()
+
+    predicted_weight = train_and_predict(height, weight, 12.0)
+    print(f"Predicted weight for height 12.0 is {predicted_weight[0]}")
