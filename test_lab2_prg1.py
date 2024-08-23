c300fcb095dfe5319c86ea34b0cb90c830a08fc0
23-AKG <131351699+23-AKG@users.noreply.github.com>
2024-08-23 06:10:30 +0530

    Update test_lab2_prg1.py

--- test_lab2_prg1.py
+++ test_lab2_prg1.py
@@ -5,10 +5,10 @@ def test_train_and_predict():
     height = [[4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0]]
     weight = [16, 25, 36, 49, 64, 81, 100]
     
-    predicted_weight = train_and_predict(height, weight, 12.0)
+    predicted_weight = train_and_predict(height, weight, 10.0)
     
-    expected_min = 120.0
-    expected_max = 146.0
+    expected_min = 90.0
+    expected_max = 110.0
     
     assert expected_min <= predicted_weight[0] <= expected_max, (
         f"Expected weight to be between {expected_min} and {expected_max}, "
